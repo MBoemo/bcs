4ab9ae974746971b128530653376f9619bcae8a7
Sarah Chapman <44526191+sarahechapman@users.noreply.github.com>
2022-11-09 14:49:51 +0000

    Update simulator.cpp

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -601,7 +601,8 @@ std::cout << "Total time elapsed: " << _totalTime << std::endl;
 
 				runningTotal = exp(log_runningTotal);
 				double lower = runningTotal / _rateSum;
-				double upper = (runningTotal + multiplier * ( (*tc) -> rate)) / _rateSum;
+				double log_upper_sum = runningTotal + ln(1 + exp(multiplier * ( (*tc) -> rate)));
+				double upper = exp(log_upper_sum) / _rateSum;
 
 				if ( uniformDraw > lower and uniformDraw <= upper ){
 #if DEBUG
